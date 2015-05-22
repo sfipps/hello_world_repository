759af43b1cf16e1f78759bbbfcb8cccc648e2d7b
Andrey <sfipps@mail.ru>
2015-05-22 11:39:18 +0300

    Hello world application - 1'st commit

---
+++ src/by/smartpro/Main.java
@@ -0,0 +1,13 @@
+package by.smartpro;
+
+public class Main {
+
+    /** Вход в программу
+     Основая функdция программы
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Привет дружок!");
+
+    }
+}
